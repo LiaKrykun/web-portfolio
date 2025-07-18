848201246bb9560e665757bd18095407a8df8370
LiaKrykun <aleksvalentinka@gmail.com>
2025-07-18 09:10:27 -0700

    changed styling and navigation

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -5,27 +5,7 @@ import { Link } from "react-router-dom";
 
 function Projects() {
   return (
-    // <div id="projects" className="projects">
-    //   <div className="projects-title">
-    //     <h1>My Projects</h1>
-    //   </div>
-    //   <div className="projects-container">
-    //     {projects.map((pj, index) => {
-    //       return (
-    //         <div className="pj-container" key={pj.pj_num || index}>
-    //           <div className="single-project">
-    //             <Link to={`/project/${pj.pj_num}`}>
-    //               <img key={index} src={pj.pj_img} alt="" />
-    //             </Link>
-    //             <Link to={`/project/${pj.pj_num}`}>
-    //               <h2>{pj.pj_name}</h2>
-    //             </Link>
-    //           </div>
-    //         </div>
-    //       );
-    //     })}
-    //   </div>
-    <div className="card-area">
+    <div id="projects" className="card-area">
       <div className="projects-title">
         <h1>My Projects</h1>
       </div>
